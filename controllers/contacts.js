e70ab6d0971117e48b4925520e3577be45a0036e
Yong <yonghwan311@gmail.com>
2023-05-08 12:36:50 -0600

    week2

---
+++ controllers/contacts.js
@@ -0,0 +1,33 @@
+const mongodb = require('../db/connect.js');
+var ObjectId = require('mongodb').ObjectId;
+
+const getContacts = async (req, res, next) => {
+  const result = await mongodb.getDb().db('CSE341').collection('contacts').find();
+
+  // console.log(result);
+  result.toArray().then((items) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(items);
+  });
+};
+
+const getSingle = async (req, res, next) => {
+  var o_id = new ObjectId(req.params.id);
+  const result = await mongodb.getDb().db('CSE341').collection('contacts').find({ _id: o_id });
+
+  result.toArray().then((items) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(items);
+  });
+};
+
+const searchContact = async (req, res, next) => {
+  var o_id = new ObjectId(req.query.id);
+  const result = await mongodb.getDb().db('CSE341').collection('contacts').find({ _id: o_id });
+  result.toArray().then((items) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.status(200).json(items);
+  });
+};
+
+module.exports = { getContacts, getSingle, searchContact };
---
+++ routes/index.js
@@ -0,0 +1,15 @@
+const routes = require('express').Router();
+
+// routes.get('/', (req, res, next) => {
+//   routes.use('/contacts', require('./contacts'));
+// });
+
+
+
+routes.get('/', (req, res, next) => {
+  res.json('Navigate to /contacts, then /contacts/{_id}, and /contacts/search?id={_id}');
+});
+
+routes.use('/contacts', require('./contacts'));
+
+module.exports = routes;
